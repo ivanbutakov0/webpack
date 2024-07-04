39ff247b31b33c87b12923b64b2a8cad9f9f28ca
Ivan Butakov <ivanbutakov000@gmail.com>
2024-07-04 17:09:21 +0500

    simple webpack setup

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { calc } from './test.js'
+
+console.log('hello world')
+console.log(calc(5, 50))
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = (env) => {
+	return {
+		mode: env.mode || 'development',
+		entry: path.resolve(__dirname, 'src', 'index.js'),
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: '[name].[contenthash].bundle.js',
+			clean: true,
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, 'public', 'index.html'),
+			}),
+			new webpack.ProgressPlugin(),
+		],
+	}
+}
