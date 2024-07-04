29cbef48b9ae589d20490667e5ce199c59b074e3
Ivan Butakov <ivanbutakov000@gmail.com>
2024-07-04 18:44:39 +0500

    add dev server to webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,18 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import path from 'path'
 import webpack from 'webpack'
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 
 type Mode = 'development' | 'production'
 
 interface EnvVariables {
 	mode: Mode
+	port: number
 }
 
 export default (env: EnvVariables) => {
+	const isDev = env.mode === 'development'
+
 	const config: webpack.Configuration = {
 		mode: env.mode || 'development',
 		entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -33,8 +37,15 @@ export default (env: EnvVariables) => {
 			new HtmlWebpackPlugin({
 				template: path.resolve(__dirname, 'public', 'index.html'),
 			}),
-			new webpack.ProgressPlugin(),
-		],
+			isDev && new webpack.ProgressPlugin(),
+		].filter(Boolean),
+		devtool: isDev && 'inline-source-map',
+		devServer: isDev
+			? ({
+					port: env.port || 3000,
+					open: true,
+			  } as DevServerConfiguration)
+			: undefined,
 	}
 	return config
 }
