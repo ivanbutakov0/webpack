114806c90d4c34712728bb8d34dba7863eb55b67
Ivan Butakov <ivanbutakov000@gmail.com>
2024-07-04 19:07:12 +0500

    setup webpack for react

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+const App = () => {
+	const [counter, setCounter] = useState(0)
+
+	const handleIncrementClick = () => setCounter((prev) => prev + 1)
+
+	return (
+		<div>
+			<h1>{counter}</h1>
+			<button type="button" onClick={handleIncrementClick}>
+				Increment
+			</button>
+		</div>
+	)
+}
+export default App
